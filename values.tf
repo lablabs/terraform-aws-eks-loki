6e40c12abaf5b4d21f1dd26e5db5b77cd5379307
Michal Muransky <michal.muransky@lablabs.io>
2022-08-23 15:53:15 +0200

    feat: make sa annotation value configurable

--- values.tf
+++ values.tf
@@ -1,13 +1,19 @@
 locals {
-  values_default = yamlencode({
-    # add default values here
+  values_default_sa_enabled = yamlencode({
+    "serviceAccount" : {
+      "annotations" : {
+        "eks.amazonaws.com/role-arn" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.this.0.name}"
+      }
+    }
   })
 }
+
+data "aws_caller_identity" "current" {}
 
 data "utils_deep_merge_yaml" "values" {
   count = var.enabled ? 1 : 0
   input = compact([
-    local.values_default,
+    var.service_account_create ? local.values_default_sa_enabled : "",
     var.values
   ])
 }
