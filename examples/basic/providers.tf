7aea429c1f99ed6cbaeeef039088e009827bcfae
Balsir <jubo.stehlik@gmail.com>
2024-01-19 10:19:25 +0100

    fix: include helm parameters when not empty

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -2,27 +2,18 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.19.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6.0"
-    }
-  }
-}
-
 data "aws_eks_cluster" "this" {
   name = module.eks_cluster.eks_cluster_id
 }
 
 data "aws_eks_cluster_auth" "this" {
   name = module.eks_cluster.eks_cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 }
 
 provider "helm" {
